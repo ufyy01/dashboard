0af1b88bf842b88773bf10cfa4ce2778b1f6f2d7
ufy <ufuomaitwa@gmail.com>
2025-01-18 23:57:15 +0100

    fixed summary card component

--- src/components/UserSummary.vue
+++ src/components/UserSummary.vue
@@ -31,7 +31,8 @@
 		<Card class="flex-grow py-6 ps-6 pr-2 bg-[#80cbc4] overflow-y-hidden">
 			<h2 class="text-sm font-semibold">Users Summary</h2>
 			<div class="flex flex-col md:flex-row h-full">
-				<div class="flex flex-col justify-end pb-3 md:w-6/12 order-2">
+				<div
+					class="flex flex-col justify-end items-end pr-4 pb-3 md:w-6/12 order-2">
 					<div class="space-y-2">
 						<div class="flex items-center">
 							<div class="w-8 h-3 rounded-full bg-blue-500"></div>
@@ -47,29 +48,28 @@
 						</div>
 					</div>
 				</div>
-				<div class="w-full h-[600px] lg:h-full">
-					<div class="w-full mx-auto mt-4 h-full relative">
+				<div class="w-full h-[400px] lg:h-full">
+					<div class="w-full mx-auto mt-2 h-full relative">
 						<div
-							class="glass w-[300px] h-[300px] rounded-full absolute right-3 top-0 flex justify-center items-center">
+							class="glass w-[240px] h-[240px] rounded-full absolute right-3 top-0 flex justify-center items-center">
 							<div
-								class="bg-blue-500 rounded-full w-[220px] h-[220px] flex flex-col justify-center items-center filter blur-lg animate-pulse"></div>
+								class="bg-blue-500 rounded-full w-[200px] h-[200px] flex flex-col justify-center items-center filter blur-lg animate-pulse"></div>
 						</div>
-						<div class="absolute right-20 top-24 text-center">
+						<!-- <div class="absolute right-24 top-28 text-center">
 							<p class="text-sm font-semibold">{{ userStore.totalCount }}</p>
 							<p class="text-xs">No of users</p>
-						</div>
+						</div> -->
 
 						<div
-							class="glass w-[150px] h-[150px] rounded-full absolute left-2 md:left-16 lg:-left-5 bottom-5 flex justify-center items-center">
+							class="glass w-[180px] h-[180px] rounded-full absolute left-2 md:left-24 bottom-5 flex justify-center items-center">
 							<div
 								class="bg-green-500 rounded-full w-[130px] h-[130px] flex flex-col justify-center items-center filter blur-lg animate-pulse"></div>
 						</div>
 
-						<div
-							class="absolute left-8 md:left-24 lg:-left-0 bottom-12 text-center">
+						<!-- <div class="absolute left-14 md:left-36 bottom-24 text-center">
 							<p class="text-sm font-semibold">{{ userStore.activeUsers }}</p>
 							<p class="text-xs">Active users</p>
-						</div>
+						</div> -->
 					</div>
 				</div>
 			</div>
